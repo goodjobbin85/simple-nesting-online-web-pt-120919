55ac5833e97fa21f2223e6947a503ac55a31b404
thomas dubiel <td1729@hotmail.com>
2019-12-19 05:42:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+  programmer_hash[0] = {
+    :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }
+  }
     
 end
 
